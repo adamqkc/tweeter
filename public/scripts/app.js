162819af197ee67d796d78bb5d0409e8b7a80927
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-08-15 21:23:36 -0700

    add ajax post request function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -66,4 +66,50 @@ $(document).ready(function () {
     }
   }
   renderTweets(tweetData);
+
+  // $("form").on("submit", function (event) {
+  //   event.preventDefault();
+  //   if ($("#textInput").val() === "") {
+  //     $("#errorMessage").text("Sorry, you can't submit an empty tweet!").slideDown().css("display", "block");
+  //     return false;
+  //   }
+  //   if ($("#textInput").val().length > 140) {
+  //     $("#errorMessage").text("Sorry, you can't submit a tweet that's longer than 140 characters!").slideDown().css("display", "block");
+  //     return false;
+  //   }
+  //   $("#errorMessage").slideUp();
+  //   var input = $("#textInput").serialize();
+
+  //   $.post("/tweets", input)
+  //     .done(function (data) {
+  //       var newTweet = createTweetElement(data);
+  //       $("#tweet-container").prepend(newTweet);
+  //       $("#textInput").val('');
+  //     });
+  // });
+  
+
+  // Tweet button is disabled by default until user gives input
+  // $('#tweet-button').prop('disabled', true);
+
+  // if ($('#text-field').val().length > 0 && $('#text-field').val().length > 140)
+
+
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+
+    if ($('#text-field') === '') {
+      alert('Please write something!');
+    } else if ($('#text-field') > 140) {
+      alert('Too many characters!');
+    } else {
+      let text = $('#text-field').serialize();
+      $.post('/tweets', text).done(function (data) {
+        var newTweet = createTweetElement(data);
+        $('#tweet-container').prepend(newTweet);
+        $('#text-field').val('');
+      });
+    };
+  });
+  
 });
