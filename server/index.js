00f860e2e36a225610a27f54eb54435475ac032b
Adam <adamqkc@gmail.com>
2018-08-17 17:07:10 -0700

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -19,29 +19,22 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
-
-
-  // The in-memory database of tweets. It's a basic object with an array in it.
-  // const db = require("./lib/in-memory-db");
   
-  // The `data-helpers` module provides an interface to the database of tweets.
-  // This simple interface layer has a big benefit: we could switch out the
-  // actual database it uses and see little to no changes elsewhere in the code
-  // (hint hint).
-  //
-  // Because it exports a function that expects the `db` as a parameter, we can
-  // require it and pass the `db` parameter immediately:
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // db.collection("tweets").find().toArray((err, results) => {
+  //   if (err) throw err;
+  //   console.log("results array: ", results);
+  // });
+
   const DataHelpers = require("./lib/data-helpers.js")(db);
-  
-  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-  // so it can define routes that use it to interact with the data layer.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
   
-  // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
+  
+  db.close();
 });
-  
-  app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
-  });
-  
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
