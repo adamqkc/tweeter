530aca17470b60e665e3e3cf5afe48a1675b872c
Adam <adamqkc@gmail.com>
2018-08-19 00:10:57 +0000

    completed mongo database

--- server/index.js
+++ server/index.js
@@ -16,17 +16,17 @@ const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
+    // db.close();
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
-
+  
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
   
   app.use("/tweets", tweetsRoutes);
   
-  db.close();
 });
 
 app.listen(PORT, () => {
